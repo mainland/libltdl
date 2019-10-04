3e27cbd04ddd55ae739e4c977f2802ee0d02ea22
Geoffrey Mainland <mainland@apeiron.net>
2019-10-04 12:49:25 -0400

    Test on GHC 8.6 and 8.8.

--- Setup.hs
+++ Setup.hs
@@ -1,8 +1,2 @@
-import Distribution.PackageDescription
 import Distribution.Simple
-import Distribution.Simple.LocalBuildInfo
-import System.FilePath
-import System.Process (system)
-
-main :: IO ()
 main = defaultMainWithHooks autoconfUserHooks
